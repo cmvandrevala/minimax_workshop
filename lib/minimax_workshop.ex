24d688b0e0da17297f7a73d7e75dda150dae8160
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2018-11-20 10:45:16 -0600

    Score a linked list of nodes

--- lib/minimax_workshop.ex
+++ lib/minimax_workshop.ex
@@ -1,2 +1,9 @@
 defmodule MinimaxWorkshop do
+  def score(node) do
+    if node.score == nil do
+      score(List.first(node.children))
+    else
+      node.score
+    end
+  end
 end
--- test/minimax_workshop_test.exs
+++ test/minimax_workshop_test.exs
@@ -1,3 +1,22 @@
 defmodule MinimaxWorkshopTest do
   use ExUnit.Case
+  import TreeNode
+
+  test "it returns the score of a node with no children" do
+    node = %TreeNode{score: -1}
+    assert MinimaxWorkshop.score(node) == -1
+  end
+
+  test "it returns the score of a node with one child" do
+    child = %TreeNode{score: 22}
+    parent = %TreeNode{children: [child]}
+    assert MinimaxWorkshop.score(parent) == 22
+  end
+
+  test "it returns the score of a linked list of nodes" do
+    grandchild = %TreeNode{score: 17}
+    child = %TreeNode{children: [grandchild]}
+    parent = %TreeNode{children: [child]}
+    assert MinimaxWorkshop.score(parent) == 17
+  end
 end
