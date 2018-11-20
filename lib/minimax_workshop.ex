72c95207e5980760045ae270ae58a7ae97700b97
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2018-11-20 10:05:48 -0600

    Use mix to initialize new project

---
+++ lib/minimax_workshop.ex
@@ -0,0 +1,18 @@
+defmodule MinimaxWorkshop do
+  @moduledoc """
+  Documentation for MinimaxWorkshop.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MinimaxWorkshop.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/minimax_workshop_test.exs
@@ -0,0 +1,8 @@
+defmodule MinimaxWorkshopTest do
+  use ExUnit.Case
+  doctest MinimaxWorkshop
+
+  test "greets the world" do
+    assert MinimaxWorkshop.hello() == :world
+  end
+end
