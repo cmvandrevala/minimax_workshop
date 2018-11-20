8594cfce2ccc3ef5fa0a719e914a5f1a9408e0c9
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2018-11-20 10:51:26 -0600

    Temporarily introduce duplication to separate min and max

--- lib/minimax_workshop.ex
+++ lib/minimax_workshop.ex
@@ -1,7 +1,25 @@
 defmodule MinimaxWorkshop do
   def score(node) do
     if node.score == nil do
-      score(List.first(node.children))
+      maximize(List.first(node.children))
+    else
+      node.score
+    end
+  end
+
+  # We introduce some duplication here to separate the differing behaviours between max and min
+
+  defp maximize(node) do
+    if node.score == nil do
+      minimize(List.first(node.children))
+    else
+      node.score
+    end
+  end
+
+  defp minimize(node) do
+    if node.score == nil do
+      maximize(List.first(node.children))
     else
       node.score
     end
